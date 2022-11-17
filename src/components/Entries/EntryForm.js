a403396c621c355ab68a37e6ed8aa02b479d7225
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-11-17 13:11:38 +0100

    posting users

--- src/components/Entries/EntryForm.js
+++ src/components/Entries/EntryForm.js
@@ -1,25 +1,56 @@
+import axios from "axios";
 import "./EntryForm.css";
+import { useState } from "react";
+
 
 const EntryForm = () => {
+    const [firstName, setFirstName] = useState();
+
+    const onFirstNameChange = (event) => {
+        setFirstName(event.target.value);
+    }
+
+    const [lastName, setLastName] = useState();
+
+    const onLastNameChange = (event) => {
+        setLastName(event.target.value);
+    }
+
+    const [phoneNumber, setPhoneNumber] = useState();
+
+    const onPhoneNumberChange = (event) => {
+        setPhoneNumber(event.target.value);
+    }
+
+    const [email, setEmail] = useState();
+
+    const onEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const postUser = () => {
+        axios.post("http://localhost:8080/users", {id:45, firstName:firstName, lastName:lastName, phoneNumber:phoneNumber, email:email});
+    }
+
     return (
         <form>
             <div className="new-entry__controls">
                 <div className="new-entry__control">
                     <label>First Name</label>
-                    <input type="text" />
+                    <input type="text" onChange={onFirstNameChange}/>
                     <label>Last Name</label>
-                    <input type="text" />
+                    <input type="text" onChange={onLastNameChange}/>
                 </div>
                 <div className="new-entry__control">
                     <label>Phone Number</label>
-                    <input type="text" />
+                    <input type="text" onChange={onPhoneNumberChange}/>
                     <label>Email</label>
-                    <input type="text" />
+                    <input type="text" onChange={onEmailChange}/>
                 </div>
                 
             </div>
             <div className="new-entry__actions">
-                    <button type="submit">Add Entry</button>
+                    <button type="submit" onClick={postUser}>Add Entry</button>
                 </div>
         </form>
     )
